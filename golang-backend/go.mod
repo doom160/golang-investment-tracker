f7a24e410731cfaa88736b85f58c353c17e834a1
Ren Fa Kay <ren_fa_kay@hive.gov.sg>
2021-09-03 17:12:29 +0800

    feat(golang): found yahoo api with date filter

--- golang-backend/go.mod
+++ golang-backend/go.mod
@@ -3,6 +3,7 @@ module main
 go 1.16
 
 require (
+	github.com/fxtlabs/date v0.0.0-20150819233934-d9ab6e2a88a9 // indirect
 	github.com/leekchan/accounting v1.0.0 // indirect
 	github.com/piquette/finance-go v1.0.0
 	github.com/shopspring/decimal v1.2.0 // indirect
--- golang-backend/main.go
+++ golang-backend/main.go
@@ -4,10 +4,25 @@ import (
     "flag"
     "fmt"
     "os"
+    "io/ioutil"
+    "net/http"
+    "github.com/fxtlabs/date"
 	"github.com/leekchan/accounting"
     "github.com/piquette/finance-go/quote"
     "github.com/sirupsen/logrus"
+    "github.com/piquette/finance-go/chart"
+    "github.com/piquette/finance-go/datetime"
 )
+// https://query2.finance.yahoo.com/v8/finance/chart/AAPL?symbol=AAPL&period1=1605796200&period2=99999999999999&interval=1d
+/*
+
+func getEpochTime() int64 {
+    return time.Today().AddDate(0,-6,0)Unix()
+}
+
+
+*/
+
 
 func main() {
     flag.Parse()
@@ -20,7 +35,14 @@ func main() {
     smbls := flag.Args()
  
     iter := quote.List(smbls)
- 
+    epoch := date.Today().UTC().AddDate(0,-6,0).Unix()
+
+    resp, err := http.Get(fmt.Sprintf("https://query2.finance.yahoo.com/v8/finance/chart/AAPL?symbol=AAPL&period1=%d&period2=99999999999999&interval=1d", epoch))
+
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    sb := string(body)
+    fmt.Printf(sb)
     for iter.Next() {
         q := iter.Quote()
         fmt.Printf("------- %v -------\n", q.ShortName)
@@ -29,4 +51,26 @@ func main() {
         fmt.Printf("52wk Low: %v\n", cf.FormatMoney(q.FiftyTwoWeekLow))
         fmt.Printf("-----------------\n")
     }
+
+	 f, err := quote.Get("SPY")
+	 if err != nil {
+	 	fmt.Println(err)
+	 } else {
+	 	fmt.Println(f)
+	 }
+
+	 params := &chart.Params{
+	 	Symbol:   "TWTR",
+	 	Interval: datetime.OneHour,
+	 }
+	 iter2 := chart.Get(params)
+	
+	 for iter2.Next() {
+	 	b := iter2.Bar()
+	 	fmt.Println(datetime.FromUnix(b.Timestamp))
+	
+	 }
+	 if iter2.Err() != nil {
+	 	fmt.Println(iter2.Err())
+	 }
 }
